Fix test failure caused by lexsort precision

mjx/mujoco/mjx/warp/test_util.py
@@ -153,7 +153,7 @@ def _mjx_efc(dx, worldid: int):
   efc_pos = select(dx._impl.efc__pos)[:nefc]
   efc_type = select(dx._impl.efc__type)[:nefc]
   efc_d = select(dx._impl.efc__D)[:nefc]
-  keys_sorted = np.lexsort((-efc_pos, efc_type, efc_d))
+  keys_sorted = np.lexsort((-np.round(efc_pos, 4), efc_type, np.round(efc_d, 4)))
   keys = keys[keys_sorted]
 
   nefc = len(keys)